4f9a1f2023a84d3d3ec0178dc01c1ee2414c3d02
Hjalte <38131968+Hjaller@users.noreply.github.com>
2023-08-17 14:00:54 +0200

    Add user class

--- Opg1_cases/Account.cs
+++ Opg1_cases/Account.cs
@@ -6,31 +6,36 @@ using System.Threading.Tasks;
 
 namespace Opg1_cases
 {
+
+    /* To do
+     * Metode til at få brugernavn
+     *
+     * 
+     * 
+     * 
+     * 
+     *
+     */
     internal class Account
     {
 
         public void Login(string username, string pass)
         {
-            
-
 
         }
 
         public bool PassStrong(string pass)
         {
-    
+
             if (pass.Length <= 12) return false;
             if (char.IsDigit(pass[0])) return false;
             if (char.IsDigit(pass[pass.Length])) return false;
             if (pass.Contains(" ")) return false;
-            
-            
+
+
 
             return true;
         }
-
-        
-
 
     }
 }
