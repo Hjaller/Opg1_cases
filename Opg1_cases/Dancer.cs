954eca043846dd9b036fb1dd84e0f7cb1f3e8d6d
Hjalte <38131968+Hjaller@users.noreply.github.com>
2023-08-17 14:04:31 +0200

    Overload operator

--- Opg1_cases/Dancer.cs
+++ Opg1_cases/Dancer.cs
@@ -16,10 +16,7 @@ namespace Opg1_cases
             this.name = name;
         }
 
-        public static int operator +(Dancer dancer1, Dancer dancer2)
-        {
-            return (int)(dancer1.points + dancer2.points); 
+        public static int operator +(Dancer dancer1, Dancer dancer2) => (int)(dancer1.points + dancer2.points);
 
-        }
     }
 }
